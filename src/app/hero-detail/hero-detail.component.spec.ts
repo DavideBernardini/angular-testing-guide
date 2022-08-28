2ec32fb7dba1c96c9a43ff9968b07860a4b73eda
DavideBernardini <davidebernardini09@gmail.com>
2022-08-28 20:53:55 +0200

    added first async test

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,5 +1,5 @@
 import { FormsModule } from '@angular/forms';
-import { ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { HeroService } from './../hero.service';
 import { ActivatedRoute } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -15,7 +15,7 @@ describe('HeroDetailComponent', () => {
   beforeEach(() => {
     //in questo caso è più semplice creare un mock a mano
     mockActivatedRoute = {
-      snapshot: {paramMap: {get: () => '3'}}
+      snapshot: { paramMap: { get: () => '3' } }
     };
     mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
     mockLocation = jasmine.createSpyObj(['back']);
@@ -25,15 +25,15 @@ describe('HeroDetailComponent', () => {
       imports: [FormsModule],
       declarations: [HeroDetailComponent],
       providers: [
-        {provide: ActivatedRoute, useValue: mockActivatedRoute},
-        {provide: HeroService, useValue: mockHeroService},
-        {provide: Location, useValue: mockLocation}
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: HeroService, useValue: mockHeroService },
+        { provide: Location, useValue: mockLocation }
       ]
     });
 
     fixture = TestBed.createComponent(HeroDetailComponent);
 
-    mockHeroService.getHero.and.returnValue(of({id:3, name: 'torpedine', strength: 100}));
+    mockHeroService.getHero.and.returnValue(of({ id: 3, name: 'torpedine', strength: 100 }));
   });
 
   //controlliamo che l'hero name sia renderizzato correttamente, controlliamo il rendering iniziale
@@ -42,4 +42,21 @@ describe('HeroDetailComponent', () => {
 
     expect(fixture.nativeElement.querySelector('h2').textContent).toContain('TORPEDINE');
   });
+
+  ////////////////////////////
+  ///////async test//////////
+  //////////////////////////
+  // utilizzo la fake async function, in pratica wrappo la callback del test con una chiamata fakeAsync in modo da trattare il nostro codice asincrono come sincrono e controlla il clock mentre il test runna. "asyncZone"
+  it('should call updateHero when save is called', fakeAsync(() => {
+    //dobbiamo dire all'heroService di ritornare un observable
+    mockHeroService.updateHero.and.returnValue(of({}));
+    fixture.detectChanges();
+
+    // chiamo il metodo save
+    fixture.componentInstance.save();
+
+    tick(250);
+
+    expect(mockHeroService.updateHero).toHaveBeenCalled();
+  }));
 });
