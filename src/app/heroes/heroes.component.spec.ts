3f0102f9b773657301f6fd973022585a93eaf242
DavideBernardini <davidebernardini09@gmail.com>
2022-08-28 19:58:10 +0200

    added route link test

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,12 +1,26 @@
 import { HeroComponent } from './../hero/hero.component';
 import { By } from '@angular/platform-browser';
 import { HeroService } from './../hero.service';
-import { Component, EventEmitter, Input, NO_ERRORS_SCHEMA, Output } from '@angular/core';
+import { Component, Directive, EventEmitter, Input, NO_ERRORS_SCHEMA, Output } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { ComponentFixture } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { HeroesComponent } from './../heroes/heroes.component';
 import { Hero } from '../hero';
+
+// creo una finta rauterLink directive per rimanere in ascolto del router link
+@Directive({
+  selector: '[routerLink]',
+  host: { '(click)': 'onClick()' }
+})
+export class RouterLinkDirectiveStub {
+  @Input('routerLink') linkParams: any;
+  navigatedTo: any = null;
+
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
 
 describe('HeroesComponent', () => {
   //definisco variabile pr il component
@@ -132,12 +146,12 @@ describe('HeroesComponent (deep tests)', () => {
     TestBed.configureTestingModule({
       declarations: [
         HeroesComponent,
-        HeroComponent
+        HeroComponent,
+        RouterLinkDirectiveStub
       ],
       providers: [
         {provide: HeroService, useValue: mockHeroService}
-      ],
-      //schemas: [NO_ERRORS_SCHEMA]
+      ]
     });
 
     fixture = TestBed.createComponent(HeroesComponent);
@@ -226,5 +240,23 @@ describe('HeroesComponent (deep tests)', () => {
     const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent
 
     expect(heroText).toContain(name);
+  });
+
+  // testo il routerlink
+  it('should have the correct route for the first hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(herosMock));
+    fixture.detectChanges();
+
+    //creo un array dei componenti child heroComponent che si trovano nel componente heroes
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+    //questo mi da un debugEwlement per l'ancor tag nel quale c'è il router link
+    //vogliamo un aggancio alla classe navigateTo quindi utilizzo injector.get che mi da una directive sul debugElement che ho passato nel tipo, RouterLinkDirectiveStub che mi ritorna la sua instance per lo specifico HeroComponent
+    let routerLink = heroComponents[0]
+      .query(By.directive(RouterLinkDirectiveStub)).injector.get(RouterLinkDirectiveStub);
+
+    heroComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+
+    expect(routerLink.navigatedTo).toBe('/detail/1');
   });
 });
